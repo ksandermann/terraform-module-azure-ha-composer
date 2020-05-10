809b81aa8347d43d18825b7b898c9037e20eb502
root <kevin.sandermann@gmail.com>
2020-05-10 12:49:24 +0000

    debug

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -69,6 +69,6 @@ resource "azurerm_lb_rule" "this" {
   frontend_ip_configuration_name = azurerm_lb.this.frontend_ip_configuration[0].name
   backend_address_pool_id        = azurerm_lb_backend_address_pool.this.id
   probe_id                       = azurerm_lb_probe.this.id
-  //false =  snat disabled
+  //true =  snat disabled
   disable_outbound_snat = true
 }
