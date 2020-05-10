5e48e6b35d5ce6cc70b6389addc52a3a10bd79fb
root <kevin.sandermann@gmail.com>
2020-05-10 11:36:11 +0000

    debug

---
+++ locals.tf
@@ -0,0 +1,11 @@
+locals {
+  systemd_svc_rendered = templatefile("${path.module}/templates/systemd-service.tmpl")
+  cloud_init_rendered = templatefile("${path.module}/templates/cloud-init.tmpl",
+    {
+      systemd_service_file_b64 = base64encode(local.systemd_svc_rendered)
+      docker_compose_file_b64  = var.docker_compose_file_b64
+      container_config_files   = var.container_config_files
+    }
+  )
+
+}
--- scaleset.tf
+++ scaleset.tf
@@ -10,15 +10,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "this" {
   disable_password_authentication = false
 
 
-  custom_data = base64encode(
-    templatefile("${path.module}/templates/cloud-init.tmpl",
-      {
-        systemd_service_file_b64 = base64encode(data.template_file.systemd_service.rendered)
-        docker_compose_file_b64  = var.docker_compose_file_b64
-        container_config_files   = var.container_config_files
-      }
-    )
-  )
+  custom_data = base64encode(local.cloud_init_rendered)
   admin_ssh_key {
     username   = var.scaleset_admin_username
     public_key = var.scaleset_admin_ssh_key
