523f4767d79ac08890048c8dc7c9fe3bdbdf34f3
root <kevin.sandermann@gmail.com>
2020-05-10 10:05:20 +0000

    debug

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "dbug_b64" {
-  value = azurerm_linux_virtual_machine_scale_set.this.custom_data
-}
-
 output "dbug" {
   value = base64decode(azurerm_linux_virtual_machine_scale_set.this.custom_data)
 }
