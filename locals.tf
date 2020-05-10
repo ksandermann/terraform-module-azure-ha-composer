58a39e4726f55ed8b563eb4bd146cc7ed0faf501
root <kevin.sandermann@gmail.com>
2020-05-10 11:39:30 +0000

    debug

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  systemd_svc_rendered = templatefile("${path.module}/templates/systemd-service.tmpl")
+  systemd_svc_rendered = templatefile("${path.module}/templates/systemd-service.tmpl", {})
   cloud_init_rendered = templatefile("${path.module}/templates/cloud-init.tmpl",
     {
       systemd_service_file_b64 = base64encode(local.systemd_svc_rendered)
